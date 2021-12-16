5aeed2984f1eac48557ba90db58c11d6d004ba4a
Gaberoids <figueiropas@hotmail.com>
2021-12-16 08:37:46 +0000

    Added the model ContactMessage to the admin app

--- contact_services/admin.py
+++ contact_services/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ContactMessage
+
+# classes below will be used to display the data on the client side
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'm_title',
+        'm_body',
+        'm_share_email_box',
+        'm_created_date',
+        'm_receiver',
+    )
+
+    ordering = ('id',)
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
