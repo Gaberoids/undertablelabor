92f5bf3cae8774db30d6655ebd47825969e17934
Gaberoids <figueiropas@hotmail.com>
2021-12-16 08:12:47 +0000

    Added functionality to delete user messeges

--- contact_services/views.py
+++ contact_services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from profiles.models import UserProfile
 from .models import ContactMessage
@@ -9,30 +9,22 @@ import gc
 
 def contact_service(request, username):
     """ Display the user's profile. """
-    print("inside contact_service---------***********-----------------**************------------")
-    print(username)
     profile = get_object_or_404(UserProfile, default_aka=username)
     # profile=admin1 = user loggedin
     # type= class = <class 'profiles.models.UserProfile'>
 
     if request.method == 'POST':
         form = MessageToServiceForm(request.POST)
-        print("below form inside of post ---------***********-----------------**************------------")
 
         if form.is_valid():
             form.save()
-            print("new_message from view contact_service ---------***********-----------------**************------------")
             messages.success(request, 'Message created successfully')
         else:
-            print("above error message form is not valid ---------***********-----------------**************------------")
             messages.error(request,
                            ('Message failed. Please ensure '
                             'the form is valid.'))
 # what the heck the next two lines are doing???
-    print("before calling form on view---------***********-----------------**************------------")
     form = MessageToServiceForm()
-    print(form)
-    print("after calling form on view---------***********-----------------**************------------")
 
     # messages_to_service = profile.orders.all()
     service = profile
@@ -44,41 +36,13 @@ def contact_service(request, username):
         'all_messages': all_messages,
         # 'on_profile_page': True
     }
-    # ???after adding the on_profile_page, go to toast to finalize it. This is for a message to let people know that the profiles was successfully changed
-    print("request---------***********-----------------**************------------")
-    print(request)
-    print("template---------***********-----------------**************------------")
-    print(template)
-    print(" context---------***********-----------------**************------------")
-    print(context)
-    print("ContactMessage.objects.all---------***********-----------------**************------------")
-    
-    classcm = ContactMessage.objects.all()
-    print(classcm)
-    print("dir()---------***********-----------------**************------------")
-    classcm1 = dir()
-    print(classcm1)
-    print("dir(classcm)---------***********-----------------**************------------")
-    classcm = dir(classcm)
-    print(classcm)
-    print("list __dir__---------***********-----------------**************------------")  
-    listdir = classcm.__dir__
-    print(listdir)
-
-    # print("vars()---------***********-----------------**************------------")
-    # classcm = vars(classcm)
-    # print(classcm)
-    
-    print("ContactMessage.objects.filter---------***********-----------------**************------------")
-    classobjid = ContactMessage.objects.filter(id=10)
-    print(classobjid)
-    
-    print("list all---------***********-----------------**************------------")
-    listall = list(classcm)
-    print(listall)
-    
-    # print("list pp.pprint(dict(os.environ))---------***********-----------------**************------------")
-    # listdir = pp.pprint(dict(classcm))
-    # print(listdir)
     return render(request, template, context)
 
+def delete_service_message(request, message_id, username):
+    """ Delete a product from the store """
+    username = username
+    template = 'contact_services/contact_service.html'
+    message_to_del = get_object_or_404(ContactMessage, pk=message_id)
+    message_to_del.delete()
+    messages.success(request, 'Message deleted!')
+    return redirect(reverse('contact_service', args=[username]))
