ee839e3aaecc37b5356fe5f4213033da0f0f7570
Gaberoids <figueiropas@hotmail.com>
2022-01-09 04:40:23 +0000

    AAdding static files into AWS

--- undertableservices/settings.py
+++ undertableservices/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'checkout',
 
     'crispy_forms',
+    'storages',
 
 ]
 
@@ -194,3 +195,27 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+if 'USE_AWS' in os.environ:
+
+    # Cache control. this tells the browser to store the cache for a long time. For performance purposes
+    # AWS_S3_OBJECT_PARAMETERS = {
+    #     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    #     'CacheControl': 'max-age=94608000',
+    # }
+    # Bucket Config.
+    AWS_STORAGE_BUCKET_NAME = 'gaberoids-undertablelabor'
+    AWS_S3_REGION_NAME = 'us-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files. this  goes with the code on custom_storage.py
+    # STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    # STATICFILES_LOCATION = 'static'
+    # DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    # MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production.
+    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
