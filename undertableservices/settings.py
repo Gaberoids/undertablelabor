b020d910dd32e7460d2f5540a43b54c87a827a42
Gaberoids <figueiropas@hotmail.com>
2022-01-13 08:35:02 +0000

    Corrected the bucket name on settings.py

--- undertableservices/settings.py
+++ undertableservices/settings.py
@@ -205,7 +205,7 @@ if 'USE_AWS' in os.environ:
     #     'CacheControl': 'max-age=94608000',
     # }
     # Bucket Config.
-    AWS_STORAGE_BUCKET_NAME = 'gaberoids-undertablelabor'
+    AWS_STORAGE_BUCKET_NAME = 'undertablelabor'
     AWS_S3_REGION_NAME = 'us-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
