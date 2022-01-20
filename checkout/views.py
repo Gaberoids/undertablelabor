f781ee49a2b1d6bf71d7430f7bc1cda6b91ae3de
Gaberoids <figueiropas@hotmail.com>
2022-01-20 10:55:32 +0000

    Only the message that will be sent will show on the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -15,7 +15,9 @@ def checkout(request):
             # 
             created_message_id = created_message.id
             # above integer id
-            # created_message_checkout = ContactMessage.objects.get(pk=created_message_id)
+            created_message_checkout = ContactMessage.objects.get(pk=created_message_id)
+            print("created_message_checkout ---------***********-----------------**************------------")
+            print(created_message_checkout)
             messages.success(request, 'Message created successfully')
         else:
             messages.error(request,
@@ -28,7 +30,7 @@ def checkout(request):
         print(x.m_title, x.m_body, x.m_sender, x.m_receiver)
     template = 'checkout/checkout.html'
     context = {
-        'all_messages': all_messages,
+        'message_to_checkout': created_message_checkout,
         # 'on_profile_page': True
     }
     return render(request, template, context)
