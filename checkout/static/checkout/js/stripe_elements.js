8b9b7af3c2beab077723dfba890770fcdd4b9106
Gaberoids <figueiropas@hotmail.com>
2022-01-27 03:19:39 +0000

    Added error message to card payment

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,10 @@
 // get text from checkout.html and slice the double commas from it
 var print_js_file = "print Js File"
-console.log(print_js_file);
-console.log("stripe_elements.js inside ---------***********-----------------**************------------")
+
+console.log('stripePublicKey  ---------***********-----------------**************------------');
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1); //slice the double quotations
+
+console.log(stripePublicKey)
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 console.log("stripe = ---------***********-----------------**************------------")
@@ -16,5 +18,23 @@ var card = elements.create('card');
 card.mount('#card-element')
 console.log("card.mount ---------***********-----------------**************------------")
 console.log(card.mount('#card-element'))
-    ;
-// .addEventListener('change', function (event) { }
+
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    // event.error = {code: '_', type: '_', message: '_'}
+    if (event.error) {
+        // event.error = {code: 'invalid_number', type: 'validation_error', message: 'Your card number is invalid.'}
+        var html = `
+            <span class="icon" role="alert">
+                <i class=''>i inside the event.error</i>
+            </span>
+            <span>${event.error.message}</span>'
+        `;
+        $(errorDiv).html(html)
+
+        console.log(event.error)
+
+    } else {
+        errorDiv.textContent = '';
+    }
+});
