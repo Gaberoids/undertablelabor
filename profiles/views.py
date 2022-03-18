24635cbd0779a75c6eed345c566f07960c07c13f
Gaberoids <figueiropas@hotmail.com>
2022-03-18 10:27:51 +0000

    Improved style of the top of the contact page, and some style on services and profile pages.

--- profiles/views.py
+++ profiles/views.py
@@ -13,7 +13,7 @@ from .forms import UserMyProfileForm
 def my_profile(request):
     """ Display the user's profile. """
     # reuqest = <WSGIRequest: GET '/profile/'>
-    profile = get_object_or_404(UserProfile, user=request.user)
+    user_profile = get_object_or_404(UserProfile, user=request.user)
     # profile=admin1 = user loggedin
     # type= class = <class 'profiles.models.UserProfile'>
     # attributes_of_profile = [item for item in accounts if item.get('id')==10][]
@@ -25,7 +25,7 @@ def my_profile(request):
         # ... 'default_aka': ['serv1'], 'default_service_provider': ['true'],
         # ...'default_town_or_city': [''], 'default_county': [''],
         # ... 'default_postcode': ['234523']}>
-        my_profile_form = UserMyProfileForm(request.POST, instance=profile)
+        my_profile_form = UserMyProfileForm(request.POST, instance=user_profile)
         if my_profile_form.is_valid():
             my_profile_form.save()
             messages.success(request, 'Profile updated successfully')
@@ -34,17 +34,16 @@ def my_profile(request):
                            ('Update failed. Please ensure '
                             'the form is valid.'))
 
-    my_profile_form = UserMyProfileForm(instance=profile)
-    orders = OrderedMessages.objects.filter(s_contact_message__m_sender=profile)
-    print(orders)
-    print("orders ---------***********-----------------**************------------")
-    message_history = ContactMessage.objects.filter(m_receiver=profile,m_sender=profile)
+    my_profile_form = UserMyProfileForm(instance=user_profile)
+    orders = OrderedMessages.objects.filter(s_contact_message__m_sender=user_profile)
+    message_history = ContactMessage.objects.filter(m_receiver=user_profile,m_sender=user_profile)
     template = 'profiles/profile.html'
     context = {
         'my_profile_form': my_profile_form,
         'orders': orders,
         'on_profile_page': True,
-        'all_messages': message_history
+        'all_messages': message_history,
+        'user_profile': user_profile
     }
     # after adding the on_profile_page, go to toast to finalize it. This is for a message to let people know that the profiles was successfully changed
     return render(request, template, context)
